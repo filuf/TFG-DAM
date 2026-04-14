ab4521b642c419870843b2c6e9c4281d786558ca
adminaitor <aitorpj93@gmail.com>
2026-04-15 00:14:58 +0200

    fix: se corrige el gestor de excepciones, gestionaba excepciones de webFlux en una aplicación web clásica debido a un copia y pega, se añade una excepción extra

--- backend/slotify/spring/src/main/java/com/slotify/backend/spring/exceptions/GlobalExceptionHandler.java
+++ backend/slotify/spring/src/main/java/com/slotify/backend/spring/exceptions/GlobalExceptionHandler.java
@@ -6,11 +6,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.bind.support.WebExchangeBindException;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -20,8 +23,21 @@ import java.util.stream.Collectors;
 @Slf4j
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(WebExchangeBindException.class)
-    public ResponseEntity<Map<String, List<String>>> handleValidationException(WebExchangeBindException exc) {
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<String> handleBadCredentiasException(BadCredentialsException exc) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(exc.getMessage());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Map<String, String>> handleReadableException(HttpMessageNotReadableException exc) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", "Cuerpo de petición inválido");
+        error.put("mensaje", "El body es obligatorio. Asegúrate de enviar un JSON válido.");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, List<String>>> handleValidationException(MethodArgumentNotValidException exc) {
         Map<String, List<String>> errors = exc.getFieldErrors().stream()
                 .collect(Collectors.groupingBy(
                         FieldError::getField,
@@ -33,14 +49,23 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(UnrecognizedPropertyException.class)
     public ResponseEntity<String> handleUnrecognizedPropertyException(UnrecognizedPropertyException exc) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body("La propiedad " + exc.getPropertyName() + " no está permitida");
+                .body("mensaje: La propiedad " + exc.getPropertyName() + " no está permitida");
+    }
+
+    @ExceptionHandler(AlreadyExistsException.class)
+    public ResponseEntity<Map<String, String>> handleAlreadyExistsException(AlreadyExistsException exc) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", "Conflicto de recursos");
+        error.put("mensaje", exc.getMessage());
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleOtherExceptions(Exception exc) {
         String errorId = UUID.randomUUID().toString();
 
-        log.error("Internal error, id error: {}", errorId, exc);
+        log.error("Internal error. id error: {}", errorId, exc);
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body("Ocurrió un error interno, por favor contacta con el equipo de desarrollo proporcionando este id " + errorId);
