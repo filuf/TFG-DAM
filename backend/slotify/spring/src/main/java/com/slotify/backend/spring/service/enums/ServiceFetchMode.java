7cc50ca547fdefd8b1707370733a5b3e5cc2f1cf
adminaitor <aitorpj93@gmail.com>
2026-04-19 19:15:37 +0200

    feat: crea un enum que representa un filtro y un mapper para los dto

---
+++ backend/slotify/spring/src/main/java/com/slotify/backend/spring/service/enums/ServiceFetchMode.java
@@ -0,0 +1,6 @@
+package com.slotify.backend.spring.service.enums;
+
+public enum ServiceFetchMode {
+    BASIC,
+    WITH_SCHEDULES
+}
--- backend/slotify/spring/src/main/java/com/slotify/backend/spring/service/mappers/ServiceMapper.java
+++ backend/slotify/spring/src/main/java/com/slotify/backend/spring/service/mappers/ServiceMapper.java
@@ -1,9 +1,16 @@
 package com.slotify.backend.spring.service.mappers;
 
+import com.slotify.backend.spring.company.dtos.GetServicesResponse;
 import com.slotify.backend.spring.company.models.CompanyEntity;
 import com.slotify.backend.spring.service.dtos.CreateServiceResponse;
+import com.slotify.backend.spring.service.dtos.ScheduleSummary;
+import com.slotify.backend.spring.service.enums.ServiceFetchMode;
 import com.slotify.backend.spring.service.models.ServiceEntity;
+import com.slotify.backend.spring.service.models.ServiceScheduleEntity;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class ServiceMapper {
@@ -24,6 +31,36 @@ public class ServiceMapper {
                 .minutesDuration(serviceEntity.getServiceMinutesDuration())
                 .priceCent(serviceEntity.getServicePriceCent())
                 .description(serviceEntity.getDescription())
+                .build();
+    }
+
+    public GetServicesResponse toGetServicesResponse(ServiceEntity serviceEntity, ServiceFetchMode fetchMode) {
+        GetServicesResponse.GetServicesResponseBuilder builder = GetServicesResponse.builder()
+                .serviceId(serviceEntity.getServiceId())
+                .description(serviceEntity.getDescription())
+                .s3ImageKey(serviceEntity.getS3ImageKey())
+                .servicePriceCent(serviceEntity.getServicePriceCent())
+                .serviceName(serviceEntity.getServiceName())
+                .serviceMinutesDuration(serviceEntity.getServiceMinutesDuration());
+
+        if (ServiceFetchMode.WITH_SCHEDULES.equals(fetchMode)) {
+            builder.schedules(this.toScheduleSummaryList(serviceEntity.getSchedules()));
+        }
+
+        return builder.build();
+    }
+
+    private List<ScheduleSummary> toScheduleSummaryList(List<ServiceScheduleEntity> scheduleEntityList) {
+        return scheduleEntityList.stream()
+                .map(this::toScheduleSummary)
+                .toList();
+    }
+    private ScheduleSummary toScheduleSummary(ServiceScheduleEntity scheduleEntity) {
+        return ScheduleSummary.builder()
+                .id(scheduleEntity.getId())
+                .dayOfWeek(scheduleEntity.getDayOfWeek())
+                .startTime(scheduleEntity.getStartTime())
+                .endTime(scheduleEntity.getEndTime())
                 .build();
     }
 }
