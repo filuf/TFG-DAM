5e433abdf7499932c0040064325c93f9bf86f1af
adminaitor <aitorpj93@gmail.com>
2026-04-19 19:17:59 +0200

    feat: crea el controlador y añade show sql al properties para comprobar el sql de las queries

---
+++ backend/slotify/spring/src/main/java/com/slotify/backend/spring/company/controllers/CompanyController.java
@@ -0,0 +1,49 @@
+package com.slotify.backend.spring.company.controllers;
+
+import com.slotify.backend.spring.company.dtos.GetServicesResponse;
+import com.slotify.backend.spring.company.useCases.GetServicesUseCase;
+import com.slotify.backend.spring.service.enums.ServiceFetchMode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/companies")
+public class CompanyController {
+
+    private static final Integer ITEMS_PER_PAGE = 10;
+    private final GetServicesUseCase getServicesUseCase;
+
+    @GetMapping("/{companyId}/services")
+    public ResponseEntity<Page<GetServicesResponse>>getServicesByCompanyId(
+            @PathVariable UUID companyId,
+            @RequestParam(defaultValue = "BASIC") ServiceFetchMode fetchMode,
+            @RequestParam(defaultValue = "0") Integer page,
+            @RequestParam(defaultValue = "name") String sortBy,
+            @RequestParam(defaultValue = "asc") String order
+    ) {
+        Sort.Direction direction = order.equalsIgnoreCase("desc")
+                ? Sort.Direction.DESC
+                : Sort.Direction.ASC;
+
+        Map<String, String> whitelistSortMap = Map.of(
+                "name", "serviceName",
+                "price", "servicePriceCent"
+        );
+        String sortField = whitelistSortMap.getOrDefault(sortBy, "serviceName");
+
+        Sort sort = Sort.by(direction, sortField);
+        Pageable pageable = PageRequest.of(page, ITEMS_PER_PAGE, sort);
+
+        Page<GetServicesResponse> response = this.getServicesUseCase.getServices(companyId, fetchMode, pageable);
+        return ResponseEntity.ok(response);
+    }
+}
