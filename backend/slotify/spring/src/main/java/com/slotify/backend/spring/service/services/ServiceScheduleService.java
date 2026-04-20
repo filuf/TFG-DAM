e9f2ca6415de773b47c2e866751ff6554b5bd2c9
adminaitor <aitorpj93@gmail.com>
2026-04-20 16:33:50 +0200

    feat: agrega el método findById en ServiceScheduleSerice

--- backend/slotify/spring/src/main/java/com/slotify/backend/spring/service/services/ServiceScheduleService.java
+++ backend/slotify/spring/src/main/java/com/slotify/backend/spring/service/services/ServiceScheduleService.java
@@ -4,9 +4,13 @@ import com.slotify.backend.spring.service.models.ServiceEntity;
 import com.slotify.backend.spring.service.models.ServiceScheduleEntity;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface ServiceScheduleService {
     List<ServiceScheduleEntity> findByServiceAndDayOfWeekIn(ServiceEntity serviceEntity, List<Integer> days);
 
     ServiceScheduleEntity saveSchedule(ServiceScheduleEntity scheduleEntity);
+
+    Optional<ServiceScheduleEntity> findById(UUID scheduleId);
 }
