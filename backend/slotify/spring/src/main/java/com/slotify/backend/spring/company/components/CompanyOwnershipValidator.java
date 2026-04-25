ba67439ca0ee5fa8349b7002c3946bb87a023983
adminaitor <aitorpj93@gmail.com>
2026-04-25 22:03:46 +0200

    feat: crea validadores para los intervales

---
+++ backend/slotify/spring/src/main/java/com/slotify/backend/spring/company/components/CompanyOwnershipValidator.java
@@ -0,0 +1,21 @@
+package com.slotify.backend.spring.company.components;
+
+import com.slotify.backend.spring.company.models.CompanyEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@Slf4j
+public class CompanyOwnershipValidator {
+
+    public void verify(UUID companyId, CompanyEntity companyEntity) {
+        if (!companyEntity.getUserId().equals(companyId)) {
+            log.warn("Un usuario de otra empresa intenta creat un intervalo de forma ilegal, usuarioMalvado: {}, empresaAtacada: {}",
+                    companyId, companyEntity.getCompanyName());
+            throw new AccessDeniedException("No posees los permisos necesarios para modificar este recurso");
+        }
+    }
+}
