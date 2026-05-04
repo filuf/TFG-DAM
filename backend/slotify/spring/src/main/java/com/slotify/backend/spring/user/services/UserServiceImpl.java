3fd2aab12d58b6c4c3f2d5cb55c3340c4759b880
adminaitor <aitorpj93@gmail.com>
2026-05-04 14:36:15 +0200

    feat: añade metodo find al servicio de usuario

--- backend/slotify/spring/src/main/java/com/slotify/backend/spring/user/services/UserServiceImpl.java
+++ backend/slotify/spring/src/main/java/com/slotify/backend/spring/user/services/UserServiceImpl.java
@@ -5,6 +5,9 @@ import com.slotify.backend.spring.user.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
@@ -14,5 +17,10 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserEntity saveUser(UserEntity userEntity) {
         return this.userRepository.save(userEntity);
+    }
+
+    @Override
+    public Optional<UserEntity> findUserById(UUID userId) {
+        return this.userRepository.findById(userId);
     }
 }
