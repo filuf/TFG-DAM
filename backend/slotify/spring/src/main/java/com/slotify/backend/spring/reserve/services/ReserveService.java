04791a207ab8a065bb6634f6e08f161cd94f667d
adminaitor <aitorpj93@gmail.com>
2026-05-06 20:35:14 +0200

    feat: agrega un método para buscar reservas por su id

--- backend/slotify/spring/src/main/java/com/slotify/backend/spring/reserve/services/ReserveService.java
+++ backend/slotify/spring/src/main/java/com/slotify/backend/spring/reserve/services/ReserveService.java
@@ -4,11 +4,13 @@ import com.slotify.backend.spring.reserve.models.ReserveEntity;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ReserveService {
 
     ReserveEntity saveReserve(ReserveEntity reserve);
+    Optional<ReserveEntity> findReserveById(UUID reserveId);
     List<ReserveEntity> findReservesByUserId(UUID userId);
     List<ReserveEntity> findAllReservesByUserIdBetweenDateTimes(UUID userId, LocalDateTime start, LocalDateTime end);
     List<ReserveEntity> findAllReservesByCompanyIdBetweenDateTimes(UUID companyId, LocalDateTime start, LocalDateTime end);
