9885328842450d16c2083216c16e87c4014166fe
rodrigo-404 <rodrigofernandezsabadias@gmail.com>
2026-05-09 21:33:39 +0200

    api: rest of services to connect with spring api

---
+++ app/src/main/java/com/raj/slotify/services/retrofit/slotifyBackend/IntervalService.kt
@@ -0,0 +1,23 @@
+package com.raj.slotify.services.retrofit.slotifyBackend
+
+import com.raj.slotify.dtos.company.CreateIntervalRequest
+import com.raj.slotify.dtos.company.CreateIntervalResponse
+import retrofit2.Response
+import retrofit2.http.*
+import java.util.UUID
+
+interface IntervalService {
+
+    @POST(".")
+    suspend fun createInterval(
+        @Header("Authorization") authHeader: String,
+        @Body createIntervalRequest: CreateIntervalRequest
+    ): Response<CreateIntervalResponse>
+
+    @DELETE("{intervalId}")
+    suspend fun deleteInterval(
+        @Header("Authorization") authHeader: String,
+        @Path("intervalId") intervalId: UUID
+    )
+
+}
---
+++ app/src/main/java/com/raj/slotify/services/retrofit/slotifyBackend/ScheduleService.kt
@@ -0,0 +1,18 @@
+package com.raj.slotify.services.retrofit.slotifyBackend
+
+import com.raj.slotify.dtos.service.PatchScheduleRequest
+import com.raj.slotify.dtos.service.ScheduleSummary
+import retrofit2.Response
+import retrofit2.http.*
+import java.util.UUID
+
+interface ScheduleService {
+
+    @PATCH("{scheduleId}")
+    suspend fun patchSchedule(
+        @Path("scheduleId") scheduleId: UUID,
+        @Header("Authorization") authHeader: String,
+        @Body request: PatchScheduleRequest
+    ): Response<ScheduleSummary>
+
+}
