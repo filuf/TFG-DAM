c83e85d014248b8caf108acfb8d4025ca6b0fa2e
unknown <rodrigofernandezsabadias@gmail.com>
2026-05-09 17:36:52 +0200

    fragment: fragments that can be called in any other

---
+++ app/src/main/java/com/example/slotify/fragments/general/FirstFragment.kt
@@ -0,0 +1,72 @@
+package com.example.slotify.fragments.general
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.findNavController
+import com.example.slotify.R
+import com.example.slotify.databinding.FragmentFirstBinding
+import com.example.slotify.viewModels.LayoutViewModel
+import com.example.slotify.viewModels.MainViewModel
+
+class FirstFragment : Fragment() {
+
+    private val viewModel: MainViewModel by activityViewModels()
+    private val layoutViewModel: LayoutViewModel by activityViewModels()
+    private lateinit var binding: FragmentFirstBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        viewModel.setNewIcon(R.drawable._logoslotify_retocado)
+        viewModel.setNewTitle(R.string.welcome)
+        viewModel.setNewExplication(R.string.account_question)
+
+        layoutViewModel.setExplicationVisibility(View.GONE)
+        layoutViewModel.setDescriptionVisibility(View.GONE)
+        layoutViewModel.setImageVisibility(View.VISIBLE)
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val textExplication: TextView = binding.explicationText
+        val changeLanguageLayout: ConstraintLayout = binding.changeLanguageLayout
+        val logOnButton: Button = binding.buttonLogOn
+        val logInButton: Button = binding.buttonLogIn
+
+        viewModel.newExplication.observe(viewLifecycleOwner) { explication ->
+            textExplication.text = getString(explication)
+        }
+
+        changeLanguageLayout.setOnClickListener {
+            view.findNavController().navigate(R.id.action_firstFragment_to_languageFragment)
+
+            viewModel.setOldTitle(viewModel.newTitle.value ?: R.string.welcome)
+        }
+
+        logOnButton.setOnClickListener {
+            view.findNavController().navigate(R.id.action_firstFragment_to_registryFragment2)
+
+            viewModel.setOldTitle(viewModel.newTitle.value ?: R.string.welcome)
+        }
+
+    }
+
+}
