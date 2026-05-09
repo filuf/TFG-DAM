d05d58ccec02db14091d53e53e1a86dd109f8640
rodrigo-404 <rodrigofernandezsabadias@gmail.com>
2026-05-09 21:16:54 +0200

    package rename: package com.example.slotify renamed to com.raj.slotify

---
+++ app/src/main/java/com/raj/slotify/dtos/registry/client/RegisterUserResponse.kt
@@ -0,0 +1,17 @@
+package com.raj.slotify.dtos.registry.client
+
+import com.google.gson.annotations.SerializedName
+import java.util.UUID
+
+data class RegisterUserResponse(
+
+    @SerializedName("userId")
+    val userId: UUID,
+
+    @SerializedName("username")
+    val username: String,
+
+    @SerializedName("email")
+    val email: String
+
+)
