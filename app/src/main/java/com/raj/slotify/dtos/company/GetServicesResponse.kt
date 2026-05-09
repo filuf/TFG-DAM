d4c2f61c0af8d572f5413bf02aa43d42bb12a898
rodrigo-404 <rodrigofernandezsabadias@gmail.com>
2026-05-09 21:13:46 +0200

    dto: companies dtos

---
+++ app/src/main/java/com/raj/slotify/dtos/company/GetServicesResponse.kt
@@ -0,0 +1,26 @@
+package com.raj.slotify.dtos.company
+
+import com.google.gson.annotations.SerializedName
+import java.util.UUID
+
+data class GetServicesResponse(
+
+    @SerializedName("serviceId")
+    val serviceId: UUID,
+
+    @SerializedName("serviceName")
+    val serviceName: String,
+
+    @SerializedName("serviceMinutesDuration")
+    val serviceMinutesDuration: Int,
+
+    @SerializedName("servicePriceCent")
+    val servicePriceCent: Int,
+
+    @SerializedName("s3ImageKey")
+    val s3ImageKey: String,
+
+    @SerializedName("description")
+    val description: String
+
+)
--- app/src/main/java/com/raj/slotify/dtos/company/RegisterCompanyRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.raj.slotify.dtos.company
-
-data class RegisterCompanyRequest(
-    var companyName: String? = null,
-    var password: String? = null,
-    var emailAddress: String? = null,
-    var defaultMaxConcurrentServices: Int? = null,
-    var physicalAddress: String? = null
-)
