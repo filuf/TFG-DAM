c709860272142b8a10146509d7b5b330ceccce9e
unknown <rodrigofernandezsabadias@gmail.com>
2026-05-09 17:54:28 +0200

    api: class that build the objet to post to spring api

---
+++ app/src/main/java/com/example/slotify/services/UserAccountManagement.kt
@@ -0,0 +1,43 @@
+package com.example.slotify.services
+
+import com.example.slotify.MainActivity
+import com.example.slotify.models.UserRegistryData
+
+class UserAccountManagement {
+
+    fun createUserAccount(userData: UserRegistryData) {
+
+        val endpoint: String = MainActivity.SPRING_ENDPOINT + "/auth/register"
+
+        if (userData.userType == "client") {
+            // CLIENTE
+
+            val clientRegistryEndpoint: String = "$endpoint/client"
+
+            val userName: String = userData.userName
+            val s3ImageKey:String = userData.s3ImageKey
+            val phoneNumber: String = userData.phoneNumber
+            val email: String = userData.email
+
+
+
+        }
+
+        else if (userData.userType == "enterprise") {
+            // EMPRESA
+
+            val enterpriseRegistryEndpoint: String = "$endpoint/company"
+
+            val defaultMaxCurrentServices: Int = userData.defaultMaxCurrentServices
+            val companyName: String = userData.companyName
+            val phoneNumber: String = userData.phoneNumber
+            val emailAddress: String = userData.emailAddress
+            val physicalAddress: String = userData.physicalAddress
+            val s3ImageKey: String = userData.s3ImageKey
+            val description: String = userData.description
+            val rattingAVG: Float = userData.rattingAVG
+        }
+
+    }
+
+}
