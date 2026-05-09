7c749a6d8c56cbbd714a93a6f17a348880b5a566
rodrigo-404 <rodrigofernandezsabadias@gmail.com>
2026-05-09 21:30:50 +0200

    finish: complete methods and packages refactor updates

---
+++ app/src/main/java/com/raj/slotify/services/retrofit/slotifyBackend/RegisterUserService.kt
@@ -0,0 +1,23 @@
+package com.raj.slotify.services.retrofit.slotifyBackend
+
+import com.raj.slotify.dtos.registry.client.RegisterUserRequest
+import com.raj.slotify.dtos.registry.client.RegisterUserResponse
+import com.raj.slotify.dtos.registry.company.RegisterCompanyRequest
+import com.raj.slotify.dtos.registry.company.RegisterCompanyResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface RegisterUserService {
+
+    @POST("register/user")
+    suspend fun registerClient(
+        @Body registerClientRequest: RegisterUserRequest
+    ): Response<RegisterUserResponse>
+
+    @POST("register/company")
+    suspend fun registerCompany(
+        @Body registerCompanyRequest: RegisterCompanyRequest
+    ): Response<RegisterCompanyResponse>
+
+}
