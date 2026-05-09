10c71172b53fcd277839520e9c437b84fd040cd6
unknown <rodrigofernandezsabadias@gmail.com>
2026-05-09 17:45:44 +0200

    model: user registry data to pass in an 'Intent' activity

---
+++ app/src/main/java/com/example/slotify/models/UserRegistryData.kt
@@ -0,0 +1,15 @@
+package com.example.slotify.models
+
+data class UserRegistryData(
+    val userType: String,
+    val defaultMaxCurrentServices: Int,
+    val companyName: String,
+    val phoneNumber: String,
+    val emailAddress: String,
+    val physicalAddress: String,
+    val s3ImageKey: String,
+    val description: String,
+    val rattingAVG: Float,
+    val userName: String,
+    val email: String
+)
