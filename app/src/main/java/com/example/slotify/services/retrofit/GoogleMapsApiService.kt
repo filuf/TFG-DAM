8567af82618a6e92bcb7f3fcd2e47bdfd152d3a4
unknown <rodrigofernandezsabadias@gmail.com>
2026-05-09 17:48:36 +0200

    api: interface service used to comunicate with google maps api

---
+++ app/src/main/java/com/example/slotify/services/retrofit/GoogleMapsApiService.kt
@@ -0,0 +1,18 @@
+package com.example.slotify.services.retrofit
+
+import com.example.slotify.fragments.mapsFragment.PlaceSuggestion
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface GoogleMapsApiService {
+
+    @GET("search")
+    suspend fun getMap(
+        @Query("q") place: String,
+        @Query("format") format: String,
+        @Header("User-Agent") userAgent: String
+    ): Response<List<PlaceSuggestion>>
+
+}
