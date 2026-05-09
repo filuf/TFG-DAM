5eb4b487388cbfb5b649b5cfa7f5a89ec36a08ee
rodrigo-404 <rodrigofernandezsabadias@gmail.com>
2026-05-09 21:31:46 +0200

    finish: complete methods and simplify imports

---
+++ app/src/main/java/com/raj/slotify/services/retrofit/slotifyBackend/CompanyService.kt
@@ -0,0 +1,19 @@
+package com.raj.slotify.services.retrofit.slotifyBackend
+
+import com.raj.slotify.dtos.maps.PlaceSuggestion
+import retrofit2.Response
+import retrofit2.http.*
+import java.util.UUID
+
+interface CompanyService {
+
+    @GET("{companyId}/services")
+    suspend fun getServicesByCompanyId(
+        @Path("companyId") companyId: UUID,
+        @Query("fetchMode") place: String,
+        @Query("page") format: String,
+        @Query("sortBy") sortBy: String,
+        @Query("order") order: String
+    ): Response<List<PlaceSuggestion>>
+
+}
