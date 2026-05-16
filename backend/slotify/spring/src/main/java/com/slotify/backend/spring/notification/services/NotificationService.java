795a67fc624e65b0df4b1957706c6e436eff283b
adminaitor <aitorpj93@gmail.com>
2026-05-16 19:52:46 +0200

    feat: crea metodo para buscar una notificacion por id y cuenta usando specification

--- backend/slotify/spring/src/main/java/com/slotify/backend/spring/notification/services/NotificationService.java
+++ backend/slotify/spring/src/main/java/com/slotify/backend/spring/notification/services/NotificationService.java
@@ -1,8 +1,16 @@
 package com.slotify.backend.spring.notification.services;
 
+import com.slotify.backend.spring.auth.enums.AccountType;
 import com.slotify.backend.spring.notification.models.NotificationEntity;
+
+import java.util.Optional;
+import java.util.UUID;
 
 public interface NotificationService {
 
     NotificationEntity saveNotification(NotificationEntity notificationEntity);
+
+    Optional<NotificationEntity> findNotificationById(UUID notificationId);
+
+    Optional<NotificationEntity> findNotificationByIdAndAccountType(UUID notificationId, AccountType accountType, UUID accountId);
 }
