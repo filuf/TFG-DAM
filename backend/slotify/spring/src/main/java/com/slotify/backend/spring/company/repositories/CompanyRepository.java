d8b7f645dc9826abf715dc42c1428300f4bb11c9
adminaitor <aitorpj93@gmail.com>
2026-05-18 20:03:47 +0200

    feat: añade repositorio y servicio para buscar una compañia

--- backend/slotify/spring/src/main/java/com/slotify/backend/spring/company/repositories/CompanyRepository.java
+++ backend/slotify/spring/src/main/java/com/slotify/backend/spring/company/repositories/CompanyRepository.java
@@ -2,9 +2,9 @@ package com.slotify.backend.spring.company.repositories;
 
 import com.slotify.backend.spring.company.models.CompanyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.UUID;
 
-public interface CompanyRepository extends JpaRepository<CompanyEntity, UUID> {
-
+public interface CompanyRepository extends JpaRepository<CompanyEntity, UUID>, JpaSpecificationExecutor<CompanyEntity> {
 }
