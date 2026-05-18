92f41409090143fcf8905061508d0bfcb61340c8
alux <aluxwastaken@gmail.com>
2026-05-18 15:43:37 +0200

    Revert "feat: agregado del endpoint de busqueda de empresa por nombre y dirección utilizando Elasticsearch"
    This reverts commit 7b64d0faa2fc1962d5886e88f0189a9ef2cafb84.

--- backend/slotify/spring/src/main/java/com/slotify/backend/spring/company/components/CompanyOwnershipValidator.java
+++ backend/slotify/spring/src/main/java/com/slotify/backend/spring/company/components/CompanyOwnershipValidator.java
@@ -10,6 +10,7 @@ import java.util.UUID;
 @Component
 @Slf4j
 public class CompanyOwnershipValidator {
+
     public void verify(UUID companyId, CompanyEntity companyEntity) {
         if (!companyEntity.getUserId().equals(companyId)) {
             log.warn("Un usuario de otra empresa intenta crear un intervalo de forma ilegal, usuarioMalvado: {}, empresaAtacada: {}",
--- backend/slotify/spring/src/main/java/com/slotify/backend/spring/company/repositories/CompanySearchRepository.java
+++
@@ -1,7 +0,0 @@
-package com.slotify.backend.spring.company.repositories;
-
-import com.slotify.backend.spring.company.models.CompanyDocument;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface CompanySearchRepository extends ElasticsearchRepository<CompanyDocument, String> {
-}
