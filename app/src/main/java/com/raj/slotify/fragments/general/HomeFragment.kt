23109450f50251516a5e03b9151f4e949dae44a4
rodrigo-404 <rodrigofernandezsabadias@gmail.com>
2026-05-24 12:33:28 +0200

    feat: fragment home

--- app/src/main/java/com/raj/slotify/fragments/general/HomeFragment.kt
+++ app/src/main/java/com/raj/slotify/fragments/general/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.raj.slotify.fragments.general
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -12,19 +13,23 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.raj.slotify.R
+import com.raj.slotify.activities.LogInActivity
 import com.raj.slotify.adapters.ReservesListCustomAdapter
 import com.raj.slotify.databinding.FragmentHomeBinding
 import com.raj.slotify.dtos.reserves.ReserveSummary
+import com.raj.slotify.models.TextModel
+import com.raj.slotify.tools.Navigation
 import com.raj.slotify.viewModels.apiRest.ReservesViewModel
+import com.raj.slotify.viewModels.frontend.ClientReservesViewModel
 import com.raj.slotify.viewModels.frontend.LayoutViewModel
 import com.raj.slotify.viewModels.frontend.MainViewModel
 import com.raj.slotify.viewModels.frontend.UserDataViewModel
-import java.time.LocalDateTime
 
 class HomeFragment : Fragment() {
 
     private val viewModel: MainViewModel by activityViewModels()
     private val userDataViewModel: UserDataViewModel by activityViewModels()
+    private val clientReservesViewModel: ClientReservesViewModel by activityViewModels()
     private val layoutViewModel: LayoutViewModel by activityViewModels()
 
     private val reservesViewModel: ReservesViewModel by activityViewModels()
@@ -39,8 +44,7 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        viewModel.setNewTitle(R.string.welcome_greeting)
-        viewModel.setSecondTitle(userDataViewModel.name.value?:"unknown user")
+        viewModel.setTitle(TextModel(R.string.welcome_greeting))
         layoutViewModel.setSecondTextVisibility(View.VISIBLE)
 
         layoutViewModel.setCenterIconTitle(true)
@@ -52,9 +56,18 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        userDataViewModel.name.observe(viewLifecycleOwner) { name ->
+            viewModel.setSecondTitle(name)
+        }
+
+        val seeMoreButton = binding.seeMoreLayout
+        seeMoreButton.setOnClickListener {
+            Navigation.navigateSafely(view.findNavController(), R.id.action_homeFragment_to_viewAllReservesFragment)
+        }
+
         val recyclerView: RecyclerView = binding.recyclerHome
-        val customAdapter = ReservesListCustomAdapter(userDataViewModel.reserves.value?:mutableListOf()) { reserve: ReserveSummary ->
-            userDataViewModel.setLastReserveSelected(reserve)
+        val customAdapter = ReservesListCustomAdapter(clientReservesViewModel.reserves.value?:mutableListOf()) { reserve: ReserveSummary ->
+            clientReservesViewModel.setLastReserveSelected(reserve)
             view.findNavController().navigate(R.id.action_homeFragment_to_reserveDetailsFragment)
         }
 
@@ -66,10 +79,31 @@ class HomeFragment : Fragment() {
                 if (response != null) {
                     if (response.isSuccessful && response.body() != null) {
                         val reservesSummary = response.body()!!.content
+
+                        val numberOfReserves = reservesSummary.size
+
+                        binding.numberReservesRemainingText.text = numberOfReserves.toString()
+                        val reserveText = if (numberOfReserves == 1) getString(R.string.reserve) else getString(R.string.reserves)
+
+                        binding.remaingText.text = "$reserveText ${getString(R.string.pending_word)}"
+
+                        clientReservesViewModel.setReserves(reservesSummary.toMutableList())
+
                         val reservesSummarySelection: MutableList<ReserveSummary> =
                             reservesSummary.take(2).toMutableList()
 
                         customAdapter.setItems(reservesSummarySelection)
+                    } else {
+                        if (response.code() == 401 && response.message().equals("Unauthorized")) {
+                            MaterialAlertDialogBuilder(requireContext())
+                                .setTitle(getString(R.string.sesion_expired_title))
+                                .setMessage(getString(R.string.sesion_expired_explication))
+                                .setPositiveButton(getString(R.string.dialog_ok)) { _, _ ->
+                                    val intent = Intent(requireActivity(), LogInActivity::class.java)
+                                    startActivity(intent)
+                                }.show()
+                        }
+                        Log.e("ERROR", "Error al obtener reservas: ${response.code()}: ${response.body()}")
                     }
                 }
             } catch (e: Exception) {
---
+++ app/src/main/java/com/raj/slotify/viewModels/frontend/ClientReservesViewModel.kt
@@ -0,0 +1,38 @@
+package com.raj.slotify.viewModels.frontend
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.raj.slotify.dtos.reserves.ReserveSummary
+import com.raj.slotify.dtos.reserves.UserReserveSummary
+
+class ClientReservesViewModel: ViewModel() {
+
+    private val _reserves = MutableLiveData<MutableList<ReserveSummary>>()
+    private val _lastReserveSelected = MutableLiveData<ReserveSummary>()
+    private val _serviceToReserve = MutableLiveData<UserReserveSummary>()
+
+    private val _categoryToReserve = MutableLiveData<String>()
+
+    val reserves: LiveData<MutableList<ReserveSummary>> = _reserves
+    val lastReserveSelected: LiveData<ReserveSummary> = _lastReserveSelected
+    val serviceToReserve: LiveData<UserReserveSummary> = _serviceToReserve
+    val categoryToReserve: LiveData<String> = _categoryToReserve
+
+    fun setReserves(reserves: MutableList<ReserveSummary>) {
+        _reserves.postValue(reserves)
+    }
+
+    fun setLastReserveSelected(lastReserveSelected: ReserveSummary) {
+        _lastReserveSelected.postValue(lastReserveSelected)
+    }
+
+    fun setServiceToReserve(serviceToReserve: UserReserveSummary) {
+        _serviceToReserve.postValue(serviceToReserve)
+    }
+
+    fun setCategoryToReserve(categoryToReserve: String) {
+        _categoryToReserve.postValue(categoryToReserve)
+    }
+
+}
