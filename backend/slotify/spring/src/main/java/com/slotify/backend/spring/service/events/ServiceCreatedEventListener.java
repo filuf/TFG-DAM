20a0b933775e0813c21f9d3c95eb4e44de7dc5ac
adminaitor <aitorpj93@gmail.com>
2026-06-06 21:48:54 +0200

    feat: indexa de forma asíncrona los servicios creados

---
+++ backend/slotify/spring/src/main/java/com/slotify/backend/spring/service/events/ServiceCreatedEventListener.java
@@ -0,0 +1,68 @@
+package com.slotify.backend.spring.service.events;
+
+
+import com.slotify.backend.spring.company.models.CompanyDocument;
+import com.slotify.backend.spring.company.models.CompanyEntity;
+import com.slotify.backend.spring.company.services.CompanySearchService;
+import com.slotify.backend.spring.service.dtos.ServiceCreatedEvent;
+import com.slotify.backend.spring.service.models.ServiceDocument;
+import com.slotify.backend.spring.service.models.ServiceEntity;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ServiceCreatedEventListener {
+
+    private final CompanySearchService companySearchService;
+
+    @Async
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void indexNewEvent(ServiceCreatedEvent event) {
+
+        CompanyEntity companyEntity = event.getCompanyEntity();
+        ServiceEntity serviceEntity = event.getServiceEntity();
+
+        CompanyDocument companyDocument = this.companySearchService.findCompanyById(companyEntity.getUserId().toString());
+
+        if (companyDocument == null) {
+            log.error("No se ha encontrado la compañía en ElasticSearch companyId: {}, No se ha podido indexar el servivio serviceId: {}",
+                    companyEntity.getUserId(), serviceEntity.getServiceId());
+            return;
+        }
+
+        ServiceDocument newService = ServiceDocument.builder()
+                .serviceId(serviceEntity.getServiceId().toString())
+                .serviceName(serviceEntity.getServiceName())
+                .description(serviceEntity.getDescription())
+                .build();
+
+
+        List<ServiceDocument> original = companyDocument.getServices();
+        if (original == null) {
+            original = List.of();
+        }
+
+        ArrayList<ServiceDocument> serviceList = new ArrayList<>(original.size() + 1);
+        serviceList.addAll(original);
+        serviceList.add(newService);
+
+        CompanyDocument newDocument = CompanyDocument.builder()
+                .companyId(companyDocument.getCompanyId())
+                .companyName(companyDocument.getCompanyName())
+                .description(companyDocument.getDescription())
+                .physicalAddress(companyDocument.getPhysicalAddress())
+                .services(serviceList)
+                .build();
+
+        this.companySearchService.indexCompany(newDocument);
+    }
+}
