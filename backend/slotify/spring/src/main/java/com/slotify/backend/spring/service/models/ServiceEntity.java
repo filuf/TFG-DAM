5d4e6ce429e677763e50d0c2c511d5ccfe80cf22
alux <aluxwastaken@gmail.com>
2026-06-07 20:29:45 +0200

    feat: permite editar un servicio

--- backend/slotify/spring/src/main/java/com/slotify/backend/spring/service/models/ServiceEntity.java
+++ backend/slotify/spring/src/main/java/com/slotify/backend/spring/service/models/ServiceEntity.java
@@ -15,6 +15,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @ToString(exclude = {"schedules", "company"})
 public class ServiceEntity {
