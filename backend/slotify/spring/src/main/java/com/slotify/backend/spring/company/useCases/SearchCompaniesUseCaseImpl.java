b25cd2c6912b4d7fd8272af7b33eb649eb7f65f9
adminaitor <aitorpj93@gmail.com>
2026-06-07 16:27:48 +0200

    Revert "Revert "feat: añade la url de la imagen s3 en el buscador de empresas""
    This reverts commit 57f6a46cb573a71a13d00612d4d79a60cfc8e587.

--- backend/slotify/spring/src/main/java/com/slotify/backend/spring/company/useCases/SearchCompaniesUseCaseImpl.java
+++ backend/slotify/spring/src/main/java/com/slotify/backend/spring/company/useCases/SearchCompaniesUseCaseImpl.java
@@ -2,6 +2,7 @@ package com.slotify.backend.spring.company.useCases;
 
 import com.slotify.backend.spring.company.dtos.SearchCompaniesResponse;
 import com.slotify.backend.spring.company.services.CompanySearchService;
+import com.slotify.backend.spring.s3.S3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,11 +13,12 @@ import org.springframework.stereotype.Service;
 public class SearchCompaniesUseCaseImpl implements SearchCompaniesUseCase {
 
     private final CompanySearchService companySearchService;
+    private final S3Service s3Service;
 
     @Override
     public Page<SearchCompaniesResponse> search(String query, Pageable pageable) {
         return this.companySearchService
                 .searchCompany(query, pageable)
-                .map(SearchCompaniesResponse::from);
+                .map(doc -> SearchCompaniesResponse.from(doc, this.s3Service.getTemporalUrl(doc.getS3ImageKey())));
     }
 }
